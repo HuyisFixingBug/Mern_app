ca26cc1b24218dae9bfea7c615a085cc02493a84
tsthuy <huydo282003@gmail.com>
2023-12-08 18:29:37 +0700

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { signUp, SignIn } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signUp);
+router.post("/sign-in", SignIn);
 export default router;

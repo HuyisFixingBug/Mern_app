29493c978df91eddf228024977e16367f8d42b7c
tsthuy <huydo282003@gmail.com>
2023-12-08 14:02:23 +0700

    create middleware handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signUp = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
@@ -8,6 +9,8 @@ export const signUp = async (req, res) => {
     await newUser.save();
     res.status(201).json("User created successfully!!!");
   } catch (error) {
-    res.status(500).json(error.message);
+    // res.status(500).json(error.message);
+    // next(errorHandler(500, error.message));
+    next(error);
   }
 };

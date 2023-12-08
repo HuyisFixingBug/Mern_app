82476cef9d1fb4cfac7a0a79d00b88313a471640
tsthuy <huydo282003@gmail.com>
2023-12-08 08:09:46 +0700

    install express and nodemon to strart the sever"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import About from './pages/About'
 import Auth from './pages/Auth'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element = {<Home />}/>
       <Route path='/sign-in' element = {<SignIn />}/>

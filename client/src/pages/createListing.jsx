0528658b9cd722ec9777b0e5c6e4474775ce4430
tsthuy <huydo282003@gmail.com>
2023-12-15 08:25:52 +0700

    complete upload image listing

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import React, { useState } from 'react'
+import { app } from '../../firebase';
+// import { space } from 'postcss/lib/list';    
 
-function createListing() {
-  return (
+    function createListing() {
+    const [files, setFiles] = useState([])
+    const [formData, setFormData] = useState({
+        imagesUrls: [],
+    })
+    const [uploadImageError, setUploadImageError] = useState(false);
+    const handleImagesSubmit = (e) =>{
+            if(files.length > 0 && files.length + formData.imagesUrls.length <7){
+                const promises = [];
+                for (let i = 0; i < files.length; i++) {
+                    promises.push(storageImage(files[i]));
+                }
+                Promise.all(promises).then((urls) =>{
+                    setFormData({...formData, imagesUrls: formData.imagesUrls.concat(urls)});
+                     setUploadImageError(false);
+                }).catch((err) =>{
+                    setUploadImageError('Image upload failed (2 mb max per image)')
+                })
+               
+
+            } else if(files.length==0) {
+                setUploadImageError('No images is chosen!!!')
+            }
+            else{
+                setUploadImageError('You can only upload 6 images per listing')
+            }
+    }  
+    const storageImage = async (file) =>{
+        return new Promise((resolve, reject) =>{
+            const storage = getStorage(app);
+            const fileName = new Date().getTime+ file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef,file);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) =>{  
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                },
+                (error) =>{
+                        reject(error)
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>{
+                        resolve(downloadURL);
+                    })
+                }
+            )   
+        })
+    }
+    console.log(formData);
+    return (
     <div>
       <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
@@ -81,13 +134,23 @@ function createListing() {
                 
                 </p>
                 <div className="flex gap-4">
-                    <input
+                    <input onChange={(e)=>{setFiles(e.target.files)}}
                     className='p-3 border cursor-pointer border-gray-300 rounded w-full'
                     type="file"  id="images" accept='image/*' multiple />
-                    <button className='text-green-700 p-3 border
+                    <button type='button' onClick={handleImagesSubmit}
+                     className='text-green-700 p-3 border
                     disabled:opacity-80
                     border-green-600 rounded-lg uppercase hover:shadow-lg hover:opacity-90'>Upload</button>
                 </div>
+               <p className='text-red-700'>{uploadImageError && uploadImageError}</p> 
+               {
+                formData.imagesUrls.length > 0 && formData.imagesUrls.map((image) =>(
+                    
+                        <img src={image} alt="listing image" className='w-20 h-20 object-cover rounded-lg' />
+                        
+                    
+                ))
+               }    
                 <button className='uppercase p-3 bg-slate-700 rounded-lg text-white hover:opacity-90 disabled:opacity-80'>Create Listing</button>
             </div>
         </form>

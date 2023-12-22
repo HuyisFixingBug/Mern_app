9e8e80d08fc0489b6161010b2208e1559763ef1a
tsthuy <huydo282003@gmail.com>
2023-12-22 21:34:08 +0700

    complete image slider

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,35 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-function Listing() {
-const [formData, setFormData] = useState([])
+// Import necessary Swiper styles
+import 'swiper/css';
+import 'swiper/css/bundle';
 
-    const params = useParams()
-    useEffect(() =>{
-        const fetchListing = async() =>{
-            try {
-            const res = await fetch(`/api/listing/get/${params.listingId}`);
-            const data = await res.json();
-            if(data.success===false){
-                console.log(data.message);
-                return;
-            }
-            setFormData(data)
-        } catch(error) {
-            console.log(error);
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules'; 
+import SwiperCore  from 'swiper'
+
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
         }
-        }
-        fetchListing()
-    }, [params.listingId])
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+
+  console.log(loading);
+
   return (
-    <div>
-      listing pages
-      {formData && (
-        <p>{ formData.name}</p>
-       
-      )
-    }
-    </div>
-  )
+    <main>
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {error && <p className='text-center my-7 text-2xl'>Something went wrong!</p>}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation slidesPerView={1} loop={false}>
+            {listing.imagesUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <img
+                  className='h-[550px] w-full object-cover'
+                  src={url}
+                  alt="listing image"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
 }
-
-export default Listing
